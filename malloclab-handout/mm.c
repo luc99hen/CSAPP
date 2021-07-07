564b8aca9df43454bd440bbadc25aa4366e5acab
luc99hen <1813927768@qq.com>
2021-07-07 15:11:28 +0800

    some changes: 1. add more test cases 2. "next-fit" strategy

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -17,6 +17,7 @@
 
 #include "mm-macro.h"
 static void* first_block;
+static const char* fit_strategy = "first";
 
 /*********************************************************
  * NOTE TO STUDENTS: Before you do anything else, please
@@ -35,11 +36,46 @@ team_t team = {
     ""
 };
 
+// print out the whole list to debug
+static void print_list() {
+    size_t block_size;
+    char* cur_block = first_block;
 
-/*
- * find_fit - find a fit free block given a size
- */
-static void* find_fit(size_t size){
+     while((block_size = GET_SIZE(HDRP(cur_block))) > 0){
+        printf("{%p, %ld, %d} ",cur_block, block_size,GET_ALLOC(HDRP(cur_block)));
+        cur_block = NEXT_BLK(cur_block);
+    }
+    printf("{%p, %ld}\n",cur_block, block_size);
+}
+
+// consistency check
+static int mm_check() {
+    size_t block_size;
+    size_t total_size = 8;  // Epilogue header + First byte
+    char* cur_block = first_block;
+    int pre_alloc = 1, cur_alloc;
+
+     while((block_size = GET_SIZE(HDRP(cur_block))) > 0){
+        cur_alloc = GET_ALLOC(HDRP(cur_block));
+        if (pre_alloc == 0 && cur_alloc == 0){
+            printf("mm_check fail: block escape coalescing\n");
+            return 0;
+        }
+        
+        cur_block = NEXT_BLK(cur_block);
+        
+        total_size += block_size;
+        pre_alloc = cur_alloc;
+    }
+
+    size_t expect_size = mem_heapsize();
+    if(total_size != expect_size){
+        printf("mm_check fail: block miss\n");
+        return 0;
+    }
+}
+
+static void* first_fit(size_t size){
     size_t block_size;
     char* cur_block = first_block;
 
@@ -49,6 +85,64 @@ static void* find_fit(size_t size){
         cur_block = NEXT_BLK(cur_block);
     }
     return NULL;
+}
+
+static void* next_fit_block;
+static void* next_fit(size_t size){
+    void* cur_block = (next_fit_block ? NEXT_BLK(next_fit_block) : NEXT_BLK(first_block));
+    size_t block_size;
+    void* loop_start = cur_block;
+
+    while(1){
+        block_size = GET_SIZE(HDRP(cur_block));
+        if(block_size == 0){ // the last block in the list, start from first
+            cur_block = first_block;
+            continue;
+        }
+
+        if(!GET_ALLOC(HDRP(cur_block)) && block_size >= size) {
+            next_fit_block = cur_block;
+            break;
+        }
+
+        cur_block = NEXT_BLK(cur_block);
+        if(cur_block == loop_start){
+            next_fit_block = cur_block = NULL;
+            break;
+        }
+    }
+
+    return cur_block;
+}
+
+static void* best_fit(size_t size){
+    size_t block_size;
+    char* cur_block = first_block;
+    size_t best_size = mem_heapsize();
+    char* best_block = NULL;
+
+     while((block_size = GET_SIZE(HDRP(cur_block))) > 0){
+        if(!GET_ALLOC(HDRP(cur_block)) && block_size >= size && block_size < best_size) {
+            best_size = block_size;
+            best_block = cur_block;
+        }
+        cur_block = NEXT_BLK(cur_block);
+    }
+    return best_block;
+}
+
+
+/*
+ * find_fit - find a fit free block given a size
+ */
+static void* find_fit(size_t size){
+   if(!strcmp(fit_strategy, "next")){
+       return next_fit(size);
+   } else if(!strcmp(fit_strategy, "best")){
+       return best_fit(size);
+   } else {
+       return first_fit(size);
+   }
 }
 
 /*
@@ -82,16 +176,22 @@ static void *coalesce(void* bp){
         return bp;
     } else if (prev_alloc && !next_alloc) {
         size += GET_SIZE(HDRP(NEXT_BLK(bp)));
+        if(NEXT_BLK(bp) == next_fit_block && !strcmp(fit_strategy, "next"))
+            next_fit_block = bp;
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
         return bp;
     } else if (!prev_alloc && next_alloc) {
         size += GET_SIZE(HDRP(PREV_BLK(bp)));
+        if(bp == next_fit_block && !strcmp(fit_strategy, "next"))    
+            next_fit_block = PREV_BLK(bp);
         PUT(FTRP(bp), PACK(size, 0));
         PUT(HDRP(PREV_BLK(bp)), PACK(size, 0));
         return PREV_BLK(bp);
     } else {
         size = size + GET_SIZE(HDRP(PREV_BLK(bp))) + GET_SIZE(HDRP(NEXT_BLK(bp)));
+        if((bp == next_fit_block || NEXT_BLK(bp) == next_fit_block) && !strcmp(fit_strategy, "next"))
+            next_fit_block = PREV_BLK(bp);
         PUT(HDRP(PREV_BLK(bp)), PACK(size, 0));
         PUT(FTRP(NEXT_BLK(bp)), PACK(size, 0));
         return PREV_BLK(bp);
@@ -140,6 +240,14 @@ int mm_init(void)
     return 0;
 }
 
+// adjust size for alignment and metadata overhead
+size_t adjust_size(size_t size){
+    if (size < DSIZE)
+        return MIN_BLOCK_SIZE;
+    else
+        return DSIZE * ((size + DSIZE + (DSIZE - 1)) / DSIZE);
+}
+
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
@@ -153,15 +261,10 @@ void *mm_malloc(size_t size)
     if(size == 0)
         return NULL;
     
-    // adjust size for alignment and metadata overhead
-    if (size < DSIZE)
-        new_size = MIN_BLOCK_SIZE;
-    else
-        new_size = DSIZE * ((size + DSIZE + (DSIZE - 1)) / DSIZE);
-    
+    new_size = adjust_size(size);
     if ((bp = find_fit(new_size)) == NULL){
         extend_size = MAX(new_size, CHUNKSIZE);
-        if ((bp = extend_heap(extend_size)) == NULL)
+        if ((bp = extend_heap(extend_size/WSIZE)) == NULL)
             return NULL;
     }
 
@@ -194,15 +297,17 @@ void *mm_realloc(void *ptr, size_t size)
     } 
 
     size_t old_size = GET_SIZE(HDRP(ptr));
-    if (size < old_size) {
-        place(ptr, size);
+    size_t new_size = adjust_size(size);
+
+    if (new_size < old_size) {
+        place(ptr, new_size);
         return ptr;
     } else {
         void* new_ptr;
-        if ((new_ptr= mm_malloc(size)) == NULL){
+        if ((new_ptr= mm_malloc(new_size)) == NULL){
             return NULL;
         }
-        memcpy(new_ptr, ptr, old_size);
+        memcpy(new_ptr, ptr, old_size-DSIZE);
         mm_free(ptr);
         return new_ptr;
     }
